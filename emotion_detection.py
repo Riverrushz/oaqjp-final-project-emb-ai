fa77f813583198f4c555327a92685c5a935ad878
Riverrushz <90576652+Riverrushz@users.noreply.github.com>
2025-11-06 02:40:31 -0500

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -10,6 +10,26 @@ def emotion_detector(text_to_analyse):
 
     formatted_response = json.loads(response.text)
 
-    emotionPredictions = formatted_response['emotionPredictions']
+    
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    dominant_emotion = 0
+    emotions = []
+    emotions[5] =  {anger_score, disgust_score, fear_score, joy_score, sadness_score}
+    emotions.sort()
+    dominant_emotion = emotions[0]
 
-    return anger_score
+
+    return {
+                'anger': anger_score,
+                'disgust': disgust_score,
+                'fear': fear_score,
+                'joy': joy_score,
+                'sadness': sadness_score,
+                'dominant_emotion': dominant_emotion
+
+}
+
